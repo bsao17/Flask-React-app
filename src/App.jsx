d424d94636c63d350237117f0cc03f0127e44a68
Bruno M <declic62gmail.com>
2023-09-18 18:37:33 +0200

    feat: update React integration and add requirements.txt

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const App = () => {
     return(
         <div>
-            App page
+            Application React + flask python
         </div>
     )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-import react from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App'
 
 const root = ReactDOM.createRoot(
@@ -7,5 +7,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-    <App/>
+    <>
+        <App/>
+    </>
 )
