6e01bdd1fdbce8cdbeebb03035177e7dbe358a0a
Bruno M <declic62gmail.com>
2023-09-18 19:07:01 +0200

    feat: add proxy localhost:5000

--- client.py
+++ client.py
@@ -1,8 +1,7 @@
+import time
+
 from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__, template_folder='build', static_folder='static')
 
 
-@app.route("/")
-def Index():
-    return render_template("index.html")
