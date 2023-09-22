6a77e45b1808e2d487414b506b3f426670eb27e1
Bruno M <declic62gmail.com>
2023-09-22 10:17:35 +0200

    feat: add login directory and files and Tailwind.css

---
+++ views/src/components/login/signin.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Signin = () => {
+return (
+    <>
+        <form>
+            <label>email</label>
+            <input HtmlFor="email" type="text" />
+            <label>email</label>
+            <input htmlFor="email" type="text" />
+            <input type="submit" value="Se connecter"/>
+        </form>
+    </>
+    )
+}
+
+export default Signin
