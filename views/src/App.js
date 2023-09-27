2be86b5cebdf375767370c1da669702b74d92864
Bruno M <declic62gmail.com>
2023-09-27 21:27:14 +0200

    feat: update views

--- views/src/App.js
+++ views/src/App.js
@@ -4,19 +4,28 @@ import axios from 'axios'
 
 function App() {
     const [data, setData] = useState()
+    const [click, setClick] = useState(true)
+    
+    const toggle_click_on = 'm-auto mt-5 bg-red-500 border border-red-500 text-white p-2 w-1/2 rounded'
+    const toggle_click_off = 'm-auto mt-5 bg-red-300 border border-red-500 text-white p-2 w-1/2 rounded'
+
     useEffect(() => {
-       axios.get('database')
-          .then(result => {
-          return setData(result.data[0][1])
-          })
-      }, []);
-    console.log(data)
+      try {
+         axios.get('/database')
+           .then(result => {
+           return setData(result.data[0][1])
+           })
+      } catch (error) {
+        console.error(error)
+      }
+      }, [data]);
+
    return (
     <div className="App flex flex-col justify-center align-center">
         <h1 id='title' className='text-2xl mt-10 mb-10'>Oh Tout Doux ... Là </h1>
 
-          <div id="form">
-            <form className='m-auto flex flex-col w-1/4'>
+          <div id="form" className='border border-black w-1/4 m-auto rounded' onSubmitCapture={()=>console.log("c'est fait !!!")}>
+            <form className='m-5 flex flex-col'>
               <div className='flex flex-col w-full'>
                 <label className='m-2' htmlFor="day_input" id='day_label'>Date</label>
                 <input type="date" id='day_input' className='border border-black rounded w-full p-2' />
@@ -31,6 +40,7 @@ function App() {
               <label className='me-5' htmlFor="day_input" id='day_label'>clôturer</label>
               <input type="checkbox" id='day_input' className='text-center' />
               </div>
+              <button className={toggle_click_on}>Enregistrer</button>
             </form>
           </div>
         <div className="contact">Contact : {data}</div>
